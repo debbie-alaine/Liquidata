939aa0981fe9b3563d6b5b444e05909a1be0059d
Deborah Sandoval <dsandoval@Deborahs-MacBook-Air.local>
2018-01-31 23:48:04 -0600

    fancy new navbar for landing page

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding, HostListener } from '@angular/core';
 import {AuthService} from '../auth/auth.service';
 import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-splash',
-  templateUrl: './splash.component.html',
-  styleUrls: ['./splash.component.scss'],
+    selector: 'app-splash',
+    templateUrl: './splash.component.html',
+    styleUrls: ['./splash.component.scss'],
     providers: [AuthService]
 })
 
+
 export class SplashComponent implements OnInit {
+
+    navStyle = {};
 
   constructor(public router: Router, private auth: AuthService) { }
 
@@ -23,9 +26,20 @@ export class SplashComponent implements OnInit {
       }
   }
 
+    @HostListener('window:scroll', [])
+    onWindowScroll() {
+        const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+        if (number > 45) {
+            this.navStyle = {'background': 'white', 'transition': 'background 2s'};
+        } else {
+            this.navStyle = {'background': 'transparent', 'transition': 'background 2s'};
+        }
+    }
+
+
   public login() {
         console.log('Logged Out Clicked');
         this.auth.login();
-    }
+  }
 
 }
