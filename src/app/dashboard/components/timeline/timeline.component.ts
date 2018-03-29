6efb0b7a14b8755c5e17665747e00076aefc3fb0
Deborah Sandoval <dsandoval@Deborahs-MacBook-Air.local>
2018-03-28 21:40:20 -0500

    like -> liked

--- src/app/dashboard/components/timeline/timeline.component.ts
+++ src/app/dashboard/components/timeline/timeline.component.ts
@@ -11,9 +11,22 @@ export class TimelineComponent implements OnInit {
     @Input() following_activity: CoActivity[];
     @Input() user_id;
 
+    liked: boolean;
+
   constructor() { }
 
   ngOnInit() {
+      this.liked = false;
+  }
+
+
+  isLiked(idElement) {
+      const element = document.getElementById(idElement);
+          if (element.innerText === 'Like ') {
+              element.innerHTML = 'Liked <i class="fa fa-thumbs-up"></i>';
+          } else {
+              element.innerHTML = 'Like <i class="fa fa-thumbs-o-up"></i>';
+          }
   }
 
 }
